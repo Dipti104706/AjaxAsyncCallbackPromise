221ee6aee5aa875c5b0b9dceebbc601fa9243c5a
Dipti104706 <diptimayeebehura104@gmail.com>
2021-11-08 14:03:58 +0530

    [Dipti] modified code to use AJAX Promise function to perform CRUD operation

--- js/AJAXPromise.js
+++ js/AJAXPromise.js
@@ -1,3 +1,4 @@
+//Uc3
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 //using promises 
 function makePromiseCall(methodType,url,async=true,data=null)
@@ -7,19 +8,21 @@ function makePromiseCall(methodType,url,async=true,data=null)
         let xhr = new XMLHttpRequest();
         xhr.onreadystatechange=function()
         {
-            //console.log("State Change Called... Ready State: "+xhr.readyState+" Status: "+xhr.status);
-            
-            if(xhr.status.toString().match('^[2][0-9]{2}$'))
+            if(xhr.readyState===4)
             {
-                resolve(xhr.responseText); //when request is success
-            }
-            else if(xhr.status.toString().match('^[4,5][0-9]{2}$'))
-            {
+                //console.log("State Change Called... Ready State: "+xhr.readyState+" Status: "+xhr.status);
+                if(xhr.status===200||xhr.status===201)
+                {
+                    resolve(xhr.responseText); //when request is success
+                }
+                else if(xhr.status>=400)
+                {
                 reject({
                 status:xhr.status,
                 statusText:xhr.statusText
                 });
                 console.log("Handle 400 client error or 500 server error");
+                }
             }    
         }
         xhr.open(methodType,url,async);
@@ -53,7 +56,6 @@ makePromiseCall("DElETE",deleteURL,false)
     console.log("user deleted:"+responseText);
 })
 .catch(error=>console.log("DELETe error status:"+JSON.stringify(error)));
-
 
 //post call
 const postURL="http://localhost:3000/employees";
